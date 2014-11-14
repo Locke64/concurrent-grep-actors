e63c70abaae1ccc67c5c1108c7a5b2b02f087ae2
Tyler <tsg7741@gmail.com>
2014-11-14 13:33:16 -0500

    Added basic example to Scan, Collection Actors

--- ScanActor.java
+++ ScanActor.java
@@ -1,3 +1,15 @@
-public class ScanActor {
-	//TODO stub
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class ScanActor extends UntypedActor{
+  LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+ 
+  public void onReceive(Object message) throws Exception {
+    if (message instanceof String) {
+      log.info("Received String message: {}", message);
+      getSender().tell(message, getSelf());
+    } else
+      unhandled(message);
+  }
 }
