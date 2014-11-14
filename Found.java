787c4195a03d27f86633ce3427b66525a201cd53
David Wilson <dmw9070@rit.edu>
2014-11-14 13:17:06 -0500

    Copy CGrep and Found from previous activity
    They shouldn't have to change at all.

--- Found.java
+++ Found.java
@@ -1,3 +1,29 @@
+import java.util.List;
+
+// Found objects contain a String with the name of the file that was searched and a List<String> with one entry in the list for each matching line.
 public class Found {
-	//TODO stub
+
+	private final String filename;
+	private final List<String> matches;
+	
+	/*Constructor
+	*
+	*contains a string with name of file searched
+	*contains List<String> with one entry in list for each matching line
+	*/
+	public Found(String filename, List<String> matches)
+	{
+		this.filename = filename;
+		this.matches = matches;
+	}
+	
+	public String getFileName()
+	{
+		return this.filename;
+	}
+	
+	public List<String> getMatches(){
+		return this.matches;
+	}
+
 }
