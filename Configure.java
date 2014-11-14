4953a5cf8d538a08c2bd9000e3beef391c501399
AcidHotpocket <rej7869@rit.edu>
2014-11-14 14:47:31 -0500

    Updated Configure.java and ScanActor.java

--- Configure.java
+++ Configure.java
@@ -1,3 +1,19 @@
+import akka.actor.ActorRef;
+
 public class Configure {
-	//TODO stub
+    private final ActorRef collection_actor;
+    private final String file_name;
+
+    public Configure(String name, ActorRef reference) {
+        file_name = name;
+	collection_actor = reference;
+    }
+
+    public ActorRef getReference() {
+	return collection_actor;
+    }
+
+    public String getFilename() {
+	return file_name;
+    }
 }
--- ScanActor.java
+++ ScanActor.java
@@ -1,15 +1,70 @@
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import java.util.LinkedList;
+import Configure.java;
+import Found.java;
+import java.util.regex.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ScanActor extends UntypedActor{
   LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+  private Configure assignment;
+  private final Pattern regExpression;
+
+  public ScanActor(Pattern expression) {
+    regExpression = expression;
+  }
  
   public void onReceive(Object message) throws Exception {
-    if (message instanceof String) {
-      log.info("Received String message: {}", message);
+    if (message instanceof Configure) {
+      log.info("Received Configure  message: {}", message);
+      assignment = message;
+      Found results = null;
+
+      if (!assignment.getFilename().equals(null)) {
+        results = searchFile();
+      } else {
+        results = searchInput();
+      }
+
       getSender().tell(message, getSelf());
     } else
       unhandled(message);
+  }
+
+  private static Found searchFile() {
+    BufferedREader reader = null;
+    LinkedList<String> matchingLines = new LinkedList<String>();
+    int lineNumber = 0;
+    String line = null;
+    Matcher lineMatcher = null;
+    Found regExResults;
+
+    try {
+      reader = new BufferedReader(new FileReader(assignment.getFilename());
+      line = reader.readLine();
+      while(line != null) {
+        lineNumber++;
+	lineMatcher = regExpression.matcher(line);
+        if (lineMatcher.find()) {
+	  matchingLines.add(lineNumber + " " + line);
+	}
+        line = reader.readLine();
+      }
+    }catch (FileNotFoundException e) {
+      //TODO exception handling
+    }catch (IOExcepion e) {
+      //TODO exeption handling
+    }
+
+    regExResults = new Found(assignment.getFilename(), matchingLines);
+    return regExResults;
+  }
+
+  private static Found searchInput() {
+    //TODO write this method
   }
 }
